3ee5b4a1b9775761fc8363070254f3dbd30cff80
shioyang <shioyang4321@gmail.com>
2016-05-17 09:32:15 +0900

    Replace index with test

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Test!' });
+  res.render('index', { title: 'JSON Formatter' });
 });
 
 module.exports = router;
